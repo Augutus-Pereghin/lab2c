141953d2ab2e022398fd149b5fcc868840213ff8
AugustusP <agp5191@psu.edu>
2020-09-10 13:46:53 +0000

    proper data type management

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <readline/readline.h>
-
 // Author: Augustus Perseghin agp5191
 //Collaborator: Kieran Murdocca kkm5754@psu.edu
 //Collaborator: Reuben Lee wzl128@psu.edu
@@ -8,26 +5,47 @@
 // Section: 4
 // Breakout: 18
 
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+
 int main(void) {
+  //variable definitions
   char* letterGrade;
-  double Grade = (double)(*readline("Enter your CMPSC 131 grade:")); 
-  if (Grade >= 93)
-    letterGrade = "A+";
-  else if (Grade >= 90)
+  char* input;
+  float Grade; 
+  
+  input = readline("Enter your CMPSC 131 grade: ");
+  Grade = atof(input);
+
+  if (Grade >= 93){
+      letterGrade = "A+";
+  }
+  else if (Grade >= 90){
     letterGrade = "A";
-  else if (Grade >= 87)
+  }
+  else if (Grade >= 87){
     letterGrade = "B+";
-  else if (Grade >= 83)
+  }
+  else if (Grade >= 83){
     letterGrade = "B";
-  else if (Grade >= 80)
+  }
+  else if (Grade >= 80){
     letterGrade = "B-";
-  else if (Grade >= 77)
+  }
+  else if (Grade >= 77){
     letterGrade = "C+";
-  else if (Grade >= 70)
+  }
+  else if (Grade >= 70){
     letterGrade = "C";
-  else if (Grade >= 60)
+  }
+  else if (Grade >= 60){
     letterGrade = "D";
-  else letterGrade = "F";
+  }
+  else {
+    letterGrade = "F";
+  }
 
   printf("Your letter grade for CMPSC 131 is %s.\n", letterGrade);
   return 0;
